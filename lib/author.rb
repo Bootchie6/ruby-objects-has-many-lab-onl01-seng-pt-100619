d114c8b4775a4ca6ca3d89d1ab3fb8ee726baf71
Jonathan Jopio <jonathanjopio@gmail.com>
2019-11-15 03:55:12 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -9,7 +9,11 @@ class Author
     Post.all.select {|post| post.author == self}
   end
   
-  def add_post
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title
     
   end
 end
